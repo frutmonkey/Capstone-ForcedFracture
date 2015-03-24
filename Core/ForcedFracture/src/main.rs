5d6bf8db7b7574c949e4b5eaa01ab85e76459f96
frutmonkey <walter@localhost.localdomain>
2015-03-24 01:50:55 +0000

    it builds, and that means i can sleep a little bit.......

--- Core/ForcedFracture/src/main.rs
+++ Core/ForcedFracture/src/main.rs
@@ -2,10 +2,14 @@ extern crate glutin;
 extern crate glium;
 
 fn main() {
-    use glium::DisplayBuild;
+	use glium::DisplayBuild;
+	
+	let display = glutin::WindowBuilder::new()
+		.with_dimensions(1024, 768)
+		.with_title(format!("Hello world"))
+		.build_glium().unwrap();
 
-    let display = glutin::WindowBuilder::new()
-        .with_dimensions(1024, 768)
-        .with_title(format!("Hello world"))
-        .build_glium().unwrap();
+	loop{
+		//keep up window
+	}
 }
