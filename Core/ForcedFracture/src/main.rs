31394c49cd51fa4ebeb449ae87349a824bf5f3fd
frutmonkey <walter@localhost.localdomain>
2015-03-25 15:31:26 +0000

    expeirmenting with glutin

--- Core/ForcedFracture/src/main.rs
+++ Core/ForcedFracture/src/main.rs
@@ -1,15 +1,21 @@
 extern crate glutin;
 extern crate glium;
 
-fn main() {
+fn main()
+{
 	use glium::DisplayBuild;
 	
-	let display = glutin::WindowBuilder::new()
+    let display = glutin::WindowBuilder::new()
 		.with_dimensions(1024, 768)
-		.with_title(format!("Hello world"))
+		.with_title(format!("Forced Fracture"))
 		.build_glium().unwrap();
 
-	loop{
-		//keep up window
-	}
+    loop
+    {
+        if display.is_closed() { 
+            return;
+        }
+        
+        
+    }
 }
