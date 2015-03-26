a36d41cdf6dfb7a5f650db85eaaa3887af8e735f
frutmonkey <frutmonkey@hotmail.com>
2015-03-26 15:03:51 +0000

    we have drawing stuffs

--- Core/ForcedFracture/src/main.rs
+++ Core/ForcedFracture/src/main.rs
@@ -32,5 +32,50 @@ fn main(){
                                      Vertex { position: [ 1.0, -1.0], tex_coords: [1.0, 0.0] }
                                  ] )
     };
-    
+     let index_buffer = glium::IndexBuffer::new(&display,glium::index::TriangleStrip(vec![1 as u16, 2, 0, 3]));
+
+    let program = glium::Program::from_source(&display, r"
+        #version 110
+        uniform mat4 matrix;
+        attribute vec2 position;
+        attribute vec2 tex_coords;
+        varying vec2 v_tex_coords;
+        void main() {
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+        v_tex_coords = tex_coords;
+        }
+        ", r"
+        #version 110
+        uniform sampler2D texture;
+        varying vec2 v_tex_coords;
+        void main() {
+        gl_FragColor = texture2D(texture, v_tex_coords);
+        }
+        ", None).unwrap();
+
+    loop{
+        let uniforms = uniform! {
+            matrix: [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32]
+            ],
+            texture: &glTex 
+        };
+        
+        let mut target = display.draw();
+            target.clear_color(0.0, 0.0, 0.0, 0.0);
+            target.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &std::default::Default::default()).unwrap();
+            target.finish();
+            // polling and handling the events received by the window
+            for event in display.poll_events() {
+                match event {
+                    glutin::Event::Closed => return,
+                    _ => ()
+                }
+            }
+
+
+    }
 }
